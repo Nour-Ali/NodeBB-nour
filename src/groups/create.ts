2107f6c7ca5425ae21ae0d3b44fecdacca3595e4
Nour Ali <ntali@andrew.cmu.ed>
2023-08-27 18:41:44 +0300

    completed validateGroupName and created interfaces

--- src/groups/create.ts
+++ src/groups/create.ts
@@ -13,36 +13,68 @@ import db from '../database';
 interface DataObject {
     name : string;
     system : boolean | string;
+    timestamp : number;
+    disableJoinRequests: number; // or boolean?
+    disableLeave: number;
+    hidden: number;
+    private: number;
+    userTitle: number;
+    userTitleEnabled: number;
+    description: string;
+    ownerUid: number;
+}
+
+interface GroupObject {
+    systemGroups : string[];
+    isPrivilegeGroup(x : string) : boolean; // eslinted
+    create(data: DataObject) : Promise<GroupData>; // implemented
+    validateGroupName(name :string) : Error | void; // implemented
+}
+
+interface GroupData {
+    name: string,
+    slug: string,
+    createtime: number,
+    userTitle: string | number,
+    userTitleEnabled: number,
+    description: string,
+    memberCount: number,
+    hidden: number,
+    system: number,
+    private: number,
+    disableJoinRequests: number,
+    disableLeave: number,
 }
 
 
-
-export default function (Groups) {
-    Groups.create = async function (data) {
-        const isSystem = isSystemGroup(data);
-        const timestamp = data.timestamp || Date.now();
-        let disableJoinRequests = parseInt(data.disableJoinRequests, 10) === 1 ? 1 : 0;
+export default function (Groups : GroupObject) {
+    Groups.create = async function (data: DataObject) : Promise<GroupData>{
+        const isSystem : boolean = isSystemGroup(data); // implemented below
+        const timestamp : number = data.timestamp || Date.now();
+        let disableJoinRequests = data.disableJoinRequests === 1 ? 1 : 0;
         if (data.name === 'administrators') {
             disableJoinRequests = 1;
         }
-        const disableLeave = parseInt(data.disableLeave, 10) === 1 ? 1 : 0;
-        const isHidden = parseInt(data.hidden, 10) === 1;
+        const disableLeave = data.disableLeave === 1 ? 1 : 0;
+        const isHidden = data.hidden === 1;
 
-        Groups.validateGroupName(data.name);
+        Groups.validateGroupName(data.name); // implemented below
 
-        const exists = await meta.userOrGroupExists(data.name);
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const exists: boolean = await meta.userOrGroupExists(data.name); // eslint?????
         if (exists) {
             throw new Error('[[error:group-already-exists]]');
         }
 
         const memberCount = data.hasOwnProperty('ownerUid') ? 1 : 0;
-        const isPrivate = data.hasOwnProperty('private') && data.private !== undefined ? parseInt(data.private, 10) === 1 : true;
+        const isPrivate = data.hasOwnProperty('private') && data.private !== undefined ? data.private === 1 : true;
         let groupData = {
             name: data.name,
             slug: slugify(data.name),
             createtime: timestamp,
             userTitle: data.userTitle || data.name,
-            userTitleEnabled: parseInt(data.userTitleEnabled, 10) === 1 ? 1 : 0,
+            userTitleEnabled: data.userTitleEnabled === 1 ? 1 : 0,
             description: data.description || '',
             memberCount: memberCount,
             hidden: isHidden ? 1 : 0,
@@ -101,7 +133,7 @@ export default function (Groups) {
         }
     }
 
-    Groups.validateGroupName = function (name) {
+    Groups.validateGroupName = function (name: string): Error | void {
         if (!name) {
             throw new Error('[[error:group-name-too-short]]');
         }
@@ -110,10 +142,14 @@ export default function (Groups) {
             throw new Error('[[error:invalid-group-name]]');
         }
 
-        if (!Groups.isPrivilegeGroup(name) && name.length > meta.config.maximumGroupNameLength) {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        if (!Groups.isPrivilegeGroup(name) && name.length > meta.config.maximumGroupNameLength) { //eslint
             throw new Error('[[error:group-name-too-long]]');
         }
 
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         if (name === 'guests' || (!Groups.isPrivilegeGroup(name) && name.includes(':'))) {
             throw new Error('[[error:invalid-group-name]]');
         }
